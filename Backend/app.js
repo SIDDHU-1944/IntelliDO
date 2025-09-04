b50de8f3d3b19dd34ed80de786b19719c1fb6bff
SIDDHU-1944 <bairaboinasiddhartha@gmail.com>
2025-09-04 12:01:33 +0530

    resolving reload issue 2

--- Backend/app.js
+++ Backend/app.js
@@ -34,7 +34,7 @@ app.get("/", (req,res)=>{
     res.send("hello world!!");
 })
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
